ebe5f8ecc941fa211e9b81da2fd71ff6fb4d7233
thanhtung6824@gmail.com <thanhtung6824@gmail.com>
2019-04-26 16:52:44 +0700

    update vector icon

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -24,6 +25,10 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Feather
+            name="bar-chart"
+            size={19}
+        />
       </View>
     );
   }
